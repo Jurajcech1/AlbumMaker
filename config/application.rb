2110786a37e0e5067dbc0aee710bf4e1c11fbd2c
Jurajcech1 <Jurajcech1@gmail.com>
2016-02-10 17:58:16 -0800

    starting to push to heroku again

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module AlbumMaker
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -21,6 +22,7 @@ module AlbumMaker
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.assets.initialize_on_precompile = false
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
